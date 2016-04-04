2505d6f32b63d3ed900ff1af6221a89d3ee60788
Vincent Mathew <vettukal@gmail.com>
2016-04-05 02:35:06 +0530

    response body

--- src/main/java/com/example/gps/GmobileController.java
+++ src/main/java/com/example/gps/GmobileController.java
@@ -24,7 +24,7 @@ public class GmobileController implements GmobileSvc {
 
 	@Override
 	@RequestMapping(value = "/submitgmobilelist", method = RequestMethod.POST)
-	public String submitGmobileList(List<Gmobile> mobile) {
+	public String submitGmobileList(@RequestBody List<Gmobile> mobile) {
 		for (Gmobile gmobile : mobile) {
 			repo.save(gmobile);
 		}
