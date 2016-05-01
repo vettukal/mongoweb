77722382a36b8d3a1b6a8c042dceda870403084f
Vincent Mathew <vettukal@gmail.com>
2016-05-01 14:52:15 +0530

    getpotholelatlngdec

--- src/main/java/com/example/gps/inter2/InterSvc.java
+++ src/main/java/com/example/gps/inter2/InterSvc.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.gps.pothole2.Pothole2;
@@ -14,6 +15,7 @@ import com.example.quiz.repository.QuizSubmission;
 import retrofit.http.Body;
 import retrofit.http.GET;
 import retrofit.http.POST;
+import retrofit.http.Query;
 
 public interface InterSvc {
 	@POST("/submitinter2")
@@ -27,5 +29,8 @@ public interface InterSvc {
 	
 	@GET("/getpotholedouble2")
     public @ResponseBody List<PotholeDouble2> getpotholedouble();
+	
+	@GET("/getpotholelatlngdec")
+    public @ResponseBody List<PotholeDouble2> getpotholelatlngdec(@Query("lattitude") Double latt, @Query("longitude") Double longi,@Query("dec") Double dec);
 	
 }
